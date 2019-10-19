ec8f6662e94ddfc0c74ad1d3310c58419d689c8a
Cezary Piątek <cezarypiatek@live.com>
2019-10-19 19:59:30 +0200

    Remove unnecessary doc

--- src/SmartCodeGenerator.Contracts/TransformationContext.cs
+++ src/SmartCodeGenerator.Contracts/TransformationContext.cs
@@ -9,13 +9,6 @@ namespace SmartCodeGenerator.Contracts
     /// </summary>
     public class TransformationContext
     {
-        /// <summary>
-        ///     Initializes a new instance of the <see cref="TransformationContext" /> class.
-        /// </summary>
-        /// <param name="document"></param>
-        /// <param name="semanticModel">The semantic model.</param>
-        /// <param name="compilation">The overall compilation being generated for.</param>
-        /// <param name="progress"></param>
         public TransformationContext(Document document, SemanticModel semanticModel,
             CSharpCompilation compilation,
             IProgress<Diagnostic> progress)
@@ -26,17 +19,12 @@ namespace SmartCodeGenerator.Contracts
             Document = document;
         }
 
-        /// <summary>
-        /// Gets the semantic model for the <see cref="Compilation" />.
-        /// </summary>
         public SemanticModel SemanticModel { get; }
 
-        /// <summary>
-        /// Gets the overall compilation being generated for.
-        /// </summary>
         public CSharpCompilation Compilation { get; }
 
         public IProgress<Diagnostic> Progress { get; }
+
         public Document Document { get; }
     }
 }

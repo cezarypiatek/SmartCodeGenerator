898ce166e6184e9d5c2cd8a97d1b95a4928c1e2a
Cezary Piątek <cezarypiatek@live.com>
2019-10-19 19:35:01 +0200

    Get rid of broken caching

--- src/SmartCodeGenerator/CompilationGenerator.cs
+++ src/SmartCodeGenerator/CompilationGenerator.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading;
@@ -18,18 +17,11 @@ namespace SmartCodeGenerator
     /// </summary>
     public class CompilationGenerator
     {
-        private const string InputAssembliesIntermediateOutputFileName = "CodeGeneration.Roslyn.InputAssemblies.txt";
         private const int ProcessCannotAccessFileHR = unchecked((int)0x80070020);
-        //TODO: Find original purpose
-        private readonly List<string> loadedAssemblies = new List<string>();
-        
         private readonly string _intermediateOutputDirectory;
         private readonly IErrorReporter _errorReporter;
-        private readonly IProgress<Diagnostic> _progress;
         private readonly DocumentTransformer _documentTransformer;
         private readonly IReadOnlyList<string> _generatorAssemblySearchPaths;
-
-      
 
         public CompilationGenerator(IReadOnlyList<string> generatorAssemblySearchPaths, string intermediateOutputDirectory, IErrorReporter errorReporter, IProgress<Diagnostic> progress)
         {
@@ -37,7 +29,6 @@ namespace SmartCodeGenerator
             _generatorAssemblySearchPaths = generatorAssemblySearchPaths;
             _intermediateOutputDirectory = intermediateOutputDirectory;
             _errorReporter = errorReporter;
-            _progress = progress;
             _documentTransformer = new DocumentTransformer(generatorPluginProvider, errorReporter, progress);
         }
 
@@ -54,42 +45,29 @@ namespace SmartCodeGenerator
                 return;
             }
 
-            var generatorAssemblyInputsFile = Path.Combine(this._intermediateOutputDirectory, InputAssembliesIntermediateOutputFileName);
-            var assembliesLastModified = GetLastModifiedAssemblyTime(generatorAssemblyInputsFile);
             await project.Documents.ProcessInParallelAsync(async document =>
             {
-                await ProcessDocument(document, assembliesLastModified, compilation, cancellationToken);
+                await ProcessDocument(document, compilation, cancellationToken);
             });
-            
-            this.SaveGeneratorAssemblyList(generatorAssemblyInputsFile);
         }
 
-        private async Task ProcessDocument(Document document, DateTime assembliesLastModified, CSharpCompilation compilation, CancellationToken cancellationToken)
+        private async Task ProcessDocument(Document document, CSharpCompilation compilation, CancellationToken cancellationToken)
         {
             cancellationToken.ThrowIfCancellationRequested();
-            var outputFilePath = GenerateOutputFilePath(_hasher.Value ?? SHA1.Create(), document.FilePath);
-            if (File.Exists(outputFilePath) == false || ShouldRegenerateFile(outputFilePath, document, assembliesLastModified))
+            var outputFilePath = GenerateOutputFilePath(document.FilePath);
+            var generatedSyntaxTree = await _documentTransformer.TransformAsync(compilation, document, cancellationToken);
+            if (generatedSyntaxTree != null)
             {
-                var generatedSyntaxTree = await _documentTransformer.TransformAsync(compilation, document, cancellationToken);
-                if (generatedSyntaxTree != null)
-                {
-                    var outputText = generatedSyntaxTree.GetText(cancellationToken);
-                    await TrySaveOutputText(outputFilePath, outputText, document, cancellationToken);
-                }
+                var outputText = generatedSyntaxTree.GetText(cancellationToken);
+                await TrySaveOutputText(outputFilePath, outputText, document, cancellationToken);
             }
         }
 
         private readonly ThreadLocal<SHA1> _hasher = new ThreadLocal<SHA1>(SHA1.Create);
 
-        private static bool ShouldRegenerateFile(string outputFilePath, Document document, DateTime assembliesLastModified)
+        private string GenerateOutputFilePath(string inputDocumentFilePath)
         {
-            var outputLastModified =  File.GetLastWriteTime(outputFilePath);
-            return File.GetLastWriteTime(document.FilePath) > outputLastModified || assembliesLastModified > outputLastModified;
-        }
-
-        private string GenerateOutputFilePath(SHA1 hasher, string inputDocumentFilePath)
-        {
-            var sourceHash = Convert.ToBase64String(hasher.ComputeHash(Encoding.UTF8.GetBytes(inputDocumentFilePath)), 0, 6)
+            var sourceHash = Convert.ToBase64String(_hasher.Value.ComputeHash(Encoding.UTF8.GetBytes(inputDocumentFilePath)), 0, 6)
                 .Replace('/', '-');
             return Path.Combine(this._intermediateOutputDirectory, Path.GetFileNameWithoutExtension(inputDocumentFilePath) + $".{sourceHash}.generated.cs");
         }
@@ -120,36 +98,6 @@ namespace SmartCodeGenerator
                     break;
                 }
             } while (true);
-        }
-
-        private static DateTime GetLastModifiedAssemblyTime(string assemblyListPath)
-        {
-            if (!File.Exists(assemblyListPath))
-            {
-                return DateTime.MinValue;
-            }
-
-            var timestamps = (File.ReadAllLines(assemblyListPath)
-                .Where(File.Exists)
-                .Select(File.GetLastWriteTime)).ToList();
-            return timestamps.Any() ? timestamps.Max() : DateTime.MinValue;
-        }
-
-        private void SaveGeneratorAssemblyList(string assemblyListPath)
-        {
-            // Union our current list with the one on disk, since our incremental code generation
-            // may have skipped some up-to-date files, resulting in fewer assemblies being loaded
-            // this time.
-            var assemblyPaths = new SortedSet<string>(StringComparer.OrdinalIgnoreCase);
-
-            if (File.Exists(assemblyListPath))
-            {
-                assemblyPaths.UnionWith(File.ReadAllLines(assemblyListPath));
-            }
-
-            assemblyPaths.UnionWith(this.loadedAssemblies);
-
-            File.WriteAllLines(assemblyListPath, assemblyPaths);
         }
     }
 }
